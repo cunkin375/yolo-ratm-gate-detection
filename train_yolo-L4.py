ee27c49c9bfef783fd8cb856f627ee0ef3edb3cf
ali <159713281+cunkin375@users.noreply.github.com>
2026-04-26 00:10:44 -0400

    add training scripts for nvidia L4 and A100 GPUs

---
+++ train_yolo-L4.py
@@ -0,0 +1,35 @@
+# pylint: disable=line-too-long, missing-docstring
+import sys
+from ultralytics import YOLO
+
+def main():
+    # Load the Nano variant of YOLO26 (NMS-free)
+    try:
+        model = YOLO("yolo26n.pt")
+    except Exception as e:
+        print(f"Error loading model: {e}")
+        sys.exit(1)
+
+    # Execute training
+    model.train(
+        data="dataset/drl_gates.yaml",
+        epochs=300,
+        save_period=10,       # Save model every 25 epochs
+        imgsz=640,
+        batch=-1,             # Use Ultralytics auto batch feature
+        optimizer="MuSGD",    # 2026 Hybrid optimizer
+        augment=True,
+        worksers=8,
+        cache='ram',          # Speed up training on Windows SSDs
+        amp=True,             # Use L4 tensor cores
+        val=True
+    )
+
+    # Export to pytorch for easy deployment
+    model.export(format="torchscript", imgsz=640)
+
+    # Export to TensorRT for optimized NVIDIA deployment
+    # model.export(format="engine", half=True, imgsz=640)
+
+if __name__ == "__main__":
+    main()
